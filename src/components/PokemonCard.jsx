3d603804a884ab12e44baaec3a0551851caa5880
matstmr <matstmr@gmail.com>
2023-09-17 10:58:36 +0200

    Utilisation de props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,5 @@
-function PokemonCard () {
-    const pokemon = pokemonList[0];
+function PokemonCard ({pokemon}) {
+    console.log(pokemon);
 
     return (
         <figure>
@@ -8,16 +8,5 @@ function PokemonCard () {
         </figure>
     );
 }
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
 export default PokemonCard;
