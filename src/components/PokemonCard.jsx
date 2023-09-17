9159afd1ae9fe2303565fae13a2485b6f6e8fe9d
matstmr <matstmr@gmail.com>
2023-09-17 09:58:26 +0200

    Ajout de la carte Bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard () {
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image de Bulbizarre" />
+            <figcaption>bulbasaur</figcaption>
+        </figure>
+    );
+}
+
+export default PokemonCard;
